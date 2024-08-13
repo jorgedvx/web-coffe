4a43623499714da8c54d782a438e15de2d8e3dcd
jorgedvx <jorgecongalopez02@gmail.com>
2024-08-13 17:32:20 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const bars = document.querySelector(".fa-bars")
+const menu = document.querySelector("#menu")
+
+
+bars.addEventListener('click', ()=>{
+
+    menu.classList.toggle("active")
+
+    // const pv1 =  menu.attributes[0].value.split()
+
+    
+
+    // if(men)
+    // menu.style.opacity = "1"
+
+})
